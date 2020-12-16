58061f8ea177185759f739a477fbce0bbeb12f9a
zuramai <ahmadsaugigis@yahoo.com>
2020-12-16 08:16:39 +0700

    Ticket management

--- C2_PHPJS/app/Http/Controllers/TicketController.php
+++ C2_PHPJS/app/Http/Controllers/TicketController.php
@@ -2,9 +2,108 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EventTicket;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Event $event)
+    {
+        return view('tickets.create', compact('event'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Event $event)
+    {
+        $request->validate([
+            'name' => 'required',
+            'cost' => 'required',
+            'special_validity' => 'required',
+        ]);
+        
+        $valids = [];
+        if($request->special_validity == 'amount') $request->validate(['amount' => 'required']);
+        else if($request->special_validity == 'date') $request->validate(['date' => 'required']);
+            
+
+        array_push($valids, [
+            "type" => $request->special_validity,
+            $request->special_validity => ($request->special_validity == 'amount' ? $request->amount : $request->date)
+        ]);
+
+        $ticket = new EventTicket;
+        $ticket->event_id = $event->id;
+        $ticket->name = $request->name;
+        $ticket->cost = $request->cost;
+        $ticket->special_validity = json_encode($valids);
+        $ticket->save();
+
+        return redirect()->route('events.show', ['event' => $event->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ticket  $ticket
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EventTicket $ticket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ticket  $ticket
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EventTicket $ticket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ticket  $ticket
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ticket $ticket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ticket  $ticket
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EventTicket $ticket)
+    {
+        //
+    }
 }
--- C2_PHPJS/resources/views/events/detail.blade.php
+++ C2_PHPJS/resources/views/events/detail.blade.php
@@ -2,26 +2,26 @@
 
 @section('sidebar')
 <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-            <div class="sidebar-sticky">
-                <ul class="nav flex-column">
-                    <li class="nav-item"><a class="nav-link" href="{{url('/')}}">Manage Events</a></li>
-                </ul>
+    <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="{{url('/')}}">Manage Events</a></li>
+        </ul>
 
-                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>{{ $event->name }}</span>
-                </h6>
-                <ul class="nav flex-column">
-                    <li class="nav-item"><a class="nav-link active" href="{{ route('events.index', ['event ' => $event->id]) }}">Overview</a></li>
-                </ul>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>{{ $event->name }}</span>
+        </h6>
+        <ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link active" href="{{ route('events.show', ['event' => $event->id]) }}">Overview</a></li>
+        </ul>
 
-                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>Reports</span>
-                </h6>
-                <ul class="nav flex-column mb-2">
-                    <li class="nav-item"><a class="nav-link" href="{{route('reports.index')}}">Room capacity</a></li>
-                </ul>
-            </div>
-        </nav>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Reports</span>
+        </h6>
+        <ul class="nav flex-column mb-2">
+            <li class="nav-item"><a class="nav-link" href="{{route('reports.index')}}">Room capacity</a></li>
+        </ul>
+    </div>
+</nav>
 @endsection
 @section('content')
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
